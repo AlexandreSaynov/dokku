a9114da5255887e1fa4920b1fec925163023d2eb
Alexandre <alexandresaynov@gmail.com>
2025-11-24 16:51:02 +0000

    changed index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
-  tagline: 'Dinosaurs are mid',
+  tagline: 'Dinosaurs are alright',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
